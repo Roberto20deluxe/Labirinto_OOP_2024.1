8ea72bea7611db60d5be11a4f4025e328bdd7f5e
Roberto Regis <125362362+Roberto20deluxe@users.noreply.github.com>
2024-05-09 22:29:55 -0300

    Create main.java

---
+++ main/main.java
@@ -0,0 +1,25 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Fechar aba quando apertar x
+		window.setResizable(false);
+		window.setTitle("Labyrinth of leaves"); //Titulo do jogo
+		
+		GamePanel gamePanel = new GamePanel();
+		window.add(gamePanel);
+		
+		window.pack(); //pack causes the window to be sized to fit the preferred size and layouts of its subcomponents
+		
+		window.setLocationRelativeTo(null); //Inicar janela no meio
+		window.setVisible(true); //Permite ver a janela
+		
+		gamePanel.startGameThread();
+	}
+
+}
